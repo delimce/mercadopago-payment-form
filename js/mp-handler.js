e64688f49e69aaff5e84b1f93a365c5d06459122
ldelima <delimce@gmail.com>
2017-08-15 17:52:29 -0400

    mercadopago custom payment form

---
+++ js/mp-handler.js
@@ -0,0 +1,164 @@
+
+
+var MP_key = 'TEST-91cf50c6-bc90-4fbf-8003-b914c164d729' ///sandbox
+
+function addEvent(el, eventName, handler) {
+    if (el.addEventListener) {
+        el.addEventListener(eventName, handler);
+    } else {
+        el.attachEvent('on' + eventName, function () {
+            handler.call(el);
+        });
+    }
+};
+
+
+
+function MPinit() {
+
+    Mercadopago.setPublishableKey(MP_key);
+    var idTypes = Mercadopago.getIdentificationTypes();
+
+}
+
+
+function getBin() {
+    var ccNumber = document.querySelector('input[data-checkout="cardNumber"]');
+    return ccNumber.value.replace(/[ .-]/g, '').slice(0, 6);
+};
+
+function guessingPaymentMethod(event) {
+    var bin = getBin();
+
+    if (event.type == "keyup") {
+        if (bin.length >= 6) {
+            Mercadopago.getPaymentMethod({
+                "bin": bin
+            }, setPaymentMethodInfo);
+        }
+    } else {
+        setTimeout(function () {
+            if (bin.length >= 6) {
+                Mercadopago.getPaymentMethod({
+                    "bin": bin
+                }, setPaymentMethodInfo);
+            }
+        }, 100);
+    }
+};
+
+function setPaymentMethodInfo(status, response) {
+    if (status == 200) {
+
+
+        // do somethings ex: show logo of the payment method
+        var form = document.querySelector('#pay');
+
+        if (document.querySelector("input[name=paymentMethodId]") == null) {
+            var paymentMethod = document.createElement('input');
+            paymentMethod.setAttribute('name', "paymentMethodId");
+            paymentMethod.setAttribute('type', "hidden");
+            paymentMethod.setAttribute('value', response[0].id);
+
+            form.appendChild(paymentMethod);
+
+            console.log(paymentMethod)
+
+        } else {
+            document.querySelector("input[name=paymentMethodId]").value = response[0].id;
+        }
+    }
+};
+
+
+
+function sdkResponseHandler(status, response) {
+
+    console.log(response);
+
+    if (status != 200 && status != 201) {
+        error_data = response;
+        var message = errorHandler(error_data.cause[0].code)
+        alert("verify filled data: " + message);
+    } else {
+
+        var form = document.querySelector('#pay');
+        var card = document.createElement('input');
+        card.setAttribute('name', "token");
+        card.setAttribute('type', "hidden");
+        card.setAttribute('value', response.id);
+        form.appendChild(card);
+        doSubmit = true;
+        //  form.submit();
+    }
+};
+
+
+
+
+function errorHandler(code) {
+
+    var message = '';
+    switch (code) {
+        case "205":
+            message = "Error, el número de la TDC no puede estar vácio"
+            break;
+
+        case "208":
+            message = "Error, mes de expiración no puede estar vácio"
+            break;
+
+
+        case "209":
+            message = "Error, año de expiración no puede estar vácio"
+            break;
+
+        case "214":
+            message = "Error, documento de identidad inválido"
+            break;
+
+
+        case "221":
+            message = "Error, el nombre del tarjetahabiente no puede ser vácio"
+            break;
+
+
+        case "325":
+            message = "Error, mes de expiración inválido"
+            break;
+
+        case "326":
+            message = "Error, año de expiración inválido"
+            break;
+
+
+        case "E301":
+            message = "Error, año de expiración inválido"
+            break;
+
+        case "E302":
+            message = "Error, código de seguridad inválido"
+            break;
+
+        default:
+            message = "Error en datos de la TDC"
+            break;
+
+    }
+
+    return message;
+
+}
+
+
+
+function doPay(event) {
+    event.preventDefault();
+    if (!doSubmit) {
+        var $form = document.querySelector('#pay');
+
+        Mercadopago.createToken($form, sdkResponseHandler); // The function "sdkResponseHandler" is defined below
+
+        return false;
+    }
+};
